5b5e26f9903001a808f94623e4960e941d924517
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-29 13:30:14 -0400

    delete functionality added

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
 const app = express()
 
 const dotenv = require('dotenv')
@@ -9,16 +11,34 @@ mongoose.connect(process.env.MONGODB_URI)
 
 const Game = require('./models/game.js')
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+app.use(methodOverride('_method'))
 
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
+app.get('/games/new', async (req, res) => {
+    res.render('./games/new.ejs')
+})
+
 app.get('/games/:gameId', async (req, res) => {
     const foundGame = await Game.findById(req.params.gameId)
     res.render('./games/show.ejs', {
+        game: foundGame
+    })
+})
+
+app.delete('/games/:gameId', async (req, res) => {
+    await Game.findByIdAndDelete(req.params.gameId)
+    res.redirect('/games')
+})
+
+app.get('/games/:gameId/edit', async (req, res) => {
+    const foundGame = await Game.findById(req.params.gameId)
+    res. render('./games/edit.ejs', {
         game: foundGame
     })
 })
@@ -41,10 +61,6 @@ app.get('/games', async (req, res) => {
     })
 })
 
-app.get('/games/new', async (req, res) => {
-    res.render('./games/new.ejs')
-})
-
 app.post('/games', async (req, res) => {
     const newGame = req.body
     newGame.upvotes = 1
@@ -58,6 +74,6 @@ mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 })
 
-app.listen(3000, () => {
-    console.log('listening on port 3000...');
+app.listen(3001, () => {
+    console.log('listening on port 3001...');
 })
